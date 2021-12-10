546092cb674fdf7077b277b3565766a93be5f095
Justin <jmohabir30@stuy.edu>
2021-12-10 14:04:57 -0500

    add stuffs

--- ALTester.java
+++ ALTester.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+
 public class ALTester{
+	ArrayList<Integer> kevin = new ArrayList<Integer>();
+	public ALTester(){
+		for(int i=0; i<23;i++){
+			kevin.add((int) (Math.random()*10));
+			}
+	}
+	
+	
+	public static void main(String[] args){
+		ALTester jonathan = new ALTester();
+		System.out.println(jonathan.kevin);
+	} 
 }
