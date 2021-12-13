2d2978884bada9349941cf90529567293efd2efd
KevinLi0711 <kli30@stuy.edu>
2021-12-12 19:21:59 -0500

    updated ALTester

--- ALTester.java
+++ ALTester.java
@@ -2,25 +2,39 @@ import java.util.ArrayList;
 
 public class ALTester{
 	ArrayList<Integer> kevin = new ArrayList<Integer>();
+
 	public ALTester(){
 		for(int i=0; i<23;i++){
 			kevin.add((int) (Math.random()*10));
-			}
+		}
 	}
 	
+	public boolean isSorted() {
+		for (int i = 1; i < 23; i++) {
+			if (!(kevin.get(i - 1) < kevin.get(i))) {
+				return false;
+			}
+		}
+		return true;
+	}
 	
 	public static void main(String[] args){
 		ALTester jonathan = new ALTester();
 		System.out.println(jonathan.kevin);
+		System.out.println(jonathan.isSorted());
 		
-		ArrayList <Integer> isSorted = new ArrayList <Integer>();
+		ALTester isSorted = new ALTester();
 		for (int i = 0; i < 23; i++){
-			isSorted.add(i);
+			isSorted.kevin.set(i, i);
 		}
+		System.out.println(isSorted.kevin);
+		System.out.println(isSorted.isSorted());
 		
-		ArrayList <Integer> isNotSorted = new ArrayList <Integer>();
+		ALTester isNotSorted = new ALTester();
 		for (int i = 0; i < 23; i++){
-			isNotSorted.add(i);
+			isNotSorted.kevin.add(i);
 		}
+		System.out.println(isNotSorted.kevin);
+		System.out.println(isNotSorted.isSorted());
 	} 
 }
